1d6f6edcc916c43801729fbdc68423828338e126
Sidhowes <sidhowes@mail.com>
2020-12-03 17:13:10 +0000

    deployment part2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn panther_fitness.wsgi:application
--- panther_fitness/settings.py
+++ panther_fitness/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'dfqr88=uu+wd33wh$ug@45-aau!n#8ubir4$bi(f=7mq&wr#g1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['panther-fitness.heroku.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'panther_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
- DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
