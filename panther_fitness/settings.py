9c1f11dcbfb4a0120a4295ae6e1b2bb3a5ffc702
Sidhowes <sidhowes@mail.com>
2020-12-03 17:28:34 +0000

    Removed secret key and set DEBUG.

--- panther_fitness/settings.py
+++ panther_fitness/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dfqr88=uu+wd33wh$ug@45-aau!n#8ubir4$bi(f=7mq&wr#g1'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['panther-fitness.herokuapp.com', 'localhost']
 
