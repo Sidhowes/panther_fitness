3fa4127b8cfaa3586c54e6c98459c97503892cbe
Sidhowes <sidhowes@mail.com>
2020-12-17 11:40:50 +0000

    deployment part3.

--- panther_fitness/settings.py
+++ panther_fitness/settings.py
@@ -178,6 +178,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'sidhowes-panther-fitness'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 60
