c66056b4cbcabd46c55d797d86d19bf1e67d59ba
Sidhowes <sidhowes@mail.com>
2020-12-17 10:53:47 +0000

    Removed secret key and set DEBUG.

--- panther_fitness/settings.py
+++ panther_fitness/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2q6vmv@$_ai6u)78vy#hk=$bbcw+a(7+llgcuk-3c2sj1^46%r'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sidhowes-panther-fitness.herokuapp.com', 'localhost']
 
@@ -134,6 +134,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
