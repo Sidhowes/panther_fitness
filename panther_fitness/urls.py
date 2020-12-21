f43e7522d9924daf12426d0d0dc0f629379af2aa
Sidhowes <sidhowes@mail.com>
2020-12-21 12:29:59 +0000

    Membership folder and html template added within main membership folder and with html and models logic in place.

--- panther_fitness/urls.py
+++ panther_fitness/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('programes/', include('programes.urls', namespace='programes')),
+    path('memberships/', include('memberships.urls', namespace='memberships')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
