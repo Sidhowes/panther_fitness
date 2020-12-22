e38191eb2ecf92ce168988ed8e17eecda90747fb
Sidhowes <sidhowes@mail.com>
2020-12-22 14:22:53 +0000

    Pyment and Transaction view implemented and parsed subscription id into function with the update transaction url created.

--- memberships/urls.py
+++ memberships/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 
-from .views import MembershipSelectView
+from .views import MembershipSelectView, PaymentView, updateTransactions
 
 app_name = 'memberships'
 
 urlpatterns = [
     path('', MembershipSelectView.as_view(), name='select'),
+    path('payment/', PaymentView, name='payment'),
+    path('update-transactions/<subscription_id>/', updateTransactions, name='update-transaction'),
 ]
--- memberships/views.py
+++ memberships/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.urls import reverse
 from django.views.generic import ListView
 
 from .models import Membership, UserMembership, Subscription
+
+import stripe
 
 
 def get_user_membership(request):
@@ -13,13 +16,20 @@ def get_user_membership(request):
         return user_membership_qs.first()
     return None
 
-
 def get_user_subscription(request):
     user_subscription_qs = Subscription.objects.filter(
         user_membership=get_user_membership(request))
     if user_subscription_qs.exists():
         user_subscription = user_subscription_qs.first()
         return user_subscription
+    return None
+
+def get_selected_membership(request):
+    membership_type = request.session['selected_membership_type']
+    selected_membership_qs = Membership.objects.filter(
+        membership_type=membership_type)
+    if selected_membership_qs.exists():
+        return selected_membership_qs.first()
     return None
 
 
@@ -54,3 +64,61 @@ class MembershipSelectView(ListView):
         request.session['selected_membership_type'] = selected_membership.membership_type
 
         return HttpResponseRedirect(reverse('memberships:payment'))
+
+
+def PaymentView(request):
+
+    user_membership = get_user_membership(request)
+
+    selected_membership = get_selected_membership(request)
+
+    publicKey = settings.STRIPE_PUBLIC_KEY
+
+    if request.method == "POST":
+        try:
+            token = request.POST['stripeToken']
+            subscription = stripe.Subscription.create(
+                customer=user_membership.stripe_customer_id,
+                items=[
+                    {
+                        "plan": selected_membership.stripe_plan_id,
+                    },
+                ],
+                source=token
+            )
+
+            return redirect(reverse('memberships:update-transactions',
+                                    kwargs={
+                                        'subscription_id': subscription.id
+                                    }))
+
+        except:
+            messages.info(request, "An error has occurred, card declined")
+
+    context = {
+        'publicKey': publicKey,
+        'selected_membership': selected_membership,
+    }
+    
+    return render(request, "memberships/membership_payment.html", context)
+
+
+def updateTransactions(request, subscription_id):
+
+    user_membership = get_user_membership(request)
+    selected_membership = get_selected_membership(request)
+    user_membership.membership = selected_membership
+    user_membership.save()
+
+    sub, created = Subscription.objects.get_or_create(user_membership=user_membership)
+    sub.stripe_subscription_id = subscription_id
+    sub.active = True
+    sub.save()
+
+    try:
+        del request.session['selected_membership_type']
+    except:
+        pass
+
+    messages.info(request, "Successfully created {} membership". format(selected_membership))
+    return redirect('/programes')
