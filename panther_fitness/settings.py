b89d29cfac27152aa361d1e536c533a439d3be0b
Sidhowes <sidhowes@mail.com>
2020-12-29 21:25:31 +0000

    DEployment - part4

--- panther_fitness/settings.py
+++ panther_fitness/settings.py
@@ -180,16 +180,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-'''
+
 if 'USE_AWS' in os.environ:
-     # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
-    
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'sidhowes-panther-fitness'
+    AWS_STORAGE_BUCKET_NAME = 'sid-howes-panther-fitness'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -204,7 +198,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-'''
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 60
